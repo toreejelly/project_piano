ac903aae5748bdbc3176fe9dd6ed9b85ba14a189
dipporee <dipporee@gmail.com>
2024-07-10 12:25:09 +0900

    생성: CommonVO 공통 사용 vo

---
+++ piano/src/main/java/com/piano/main/CommonVO.java
@@ -0,0 +1,21 @@
+package com.piano.main;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class CommonVO {
+
+	// 공통
+	private long regUserSeq; // 사용자 순서
+	private Timestamp regDt; // 등록일
+	private long modiUserSeq; // 수정자 순서
+	private Timestamp modiDt; // 수정 날짜
+	private long delUserSeq; // 삭제자 순서
+	private Timestamp delDt; // 삭제일
+	private String delYn; // 삭제 여부
+
+}
